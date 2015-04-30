64b34c21662b50d4c9c5e930039e9e7e2a042c25
Johnny <wangchianing@gmail.com>
2015-04-29 22:06:03 -0700

    Update novavmdelete.py

--- novavmdelete.py
+++ novavmdelete.py
@@ -8,20 +8,6 @@ import subprocess
 vmstring="fio-driver-"
 volstring="fio-driver-"
 
-#nova image-list
-imagestring="trusty-server-cloudimg-amd64-disk"
-
-#nova flavor-list
-flavorstring="m1.small"
-
-#controller has 2 nic, public and private
-#slaves has only 1 nic, private
-#neutron net-list
-#public-direct-604 = 191a83f2-3de2-43f8-8ea5-b95ae3defd6e
-nic_id1string="191a83f2-3de2-43f8-8ea5-b95ae3defd6e"
-#private-net-3006 = 556b731a-6c12-47fe-89c5-662d1ed440d8
-nic_id2string="556b731a-6c12-47fe-89c5-662d1ed440d8"
-
 VM_NAMEstring="fio-driver-"
 
 start=input("please give a fio-drive-## start:")
